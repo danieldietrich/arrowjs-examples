ac78d57a4f3842cc1272c3df133725740cb29002
Daniel Dietrich <cafebab3@gmail.com>
2020-10-11 19:31:23 +0200

    Added react and preact examples for sierpinski triangle

---
+++ public/sierpinski-triangle-react/precache-manifest.a5ce5da727759e0485ac18987c2d90cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c649cb6a5ceabf721d4258c30cab4ad5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "391ea8002f45f3c5d74a",
+    "url": "./static/css/main.b2b766a1.chunk.css"
+  },
+  {
+    "revision": "c58564c3c39b279f425d",
+    "url": "./static/js/2.4a890b0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4a890b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391ea8002f45f3c5d74a",
+    "url": "./static/js/main.f685e7d5.chunk.js"
+  },
+  {
+    "revision": "88baf53030a657a1e378",
+    "url": "./static/js/runtime-main.eb1b5bf3.js"
+  }
+]);
